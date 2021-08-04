5d8478d2bdb085209b6325e6faf520a38861de85
s-vsp <kkwarciak@student.agh.edu.pl>
2021-08-05 00:57:34 +0200

    trained - generated album LIGHTS

--- test_file.py
+++ test_file.py
@@ -1,2 +1,6 @@
-print(1)
-print(32)
+for song, chars_num in enumerate(range(10), [1500, 1700, 2000, 2100, 2000, 1900, 2300, 2150, 1600, 2000]):
+        
+        if song % 2 == 0:
+            print("PARZYSTE")
+        else:
+            print("NIEPARZYSTE")
